694531a89f592025407b8a964c8e46d32616ba3c
DESKTOP-3FBSC30\HP <msaud.zeesofttech@outlook.com>
2025-11-10 11:16:10 +0500

    Fix: Alies issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,17 +6,13 @@ plugins {
 
 android {
     namespace = "com.example.custombutton"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 24
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
-    buildFeatures { viewBinding = true }
 
     buildTypes {
         release {
@@ -27,37 +23,32 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
-publishing {
-    publications {
-        create<MavenPublication>("release") {
-            groupId = "com.github.mohammadsauddeveloper"
-            artifactId = "CustomButton"
-            version = "v1.0.0"
 
-            afterEvaluate {
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
                 from(components["release"])
+                groupId = "com.github.mohammadsauddeveloper"
+                artifactId = "CustomButton"
+                version = "1.0.0"
             }
         }
     }
 }
-
-
